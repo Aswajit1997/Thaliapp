0f351e34b28ba5bd11261f1f27384d90e1af6dfc
Aswajit Mohapatra <aswajit.cool123@gmail.com>
2022-12-21 19:14:33 +0530

    thaliapp

---
+++ src/Components/Cart.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import {useSelector,useDispatch} from "react-redux"
+import {remove,decrement,increment} from "./productSlice"
+
+const Cart = () => {
+
+  
+//   const [total,setTotal]=useState(0)
+  
+  const Dispatch=useDispatch()
+
+  const ProductItem=useSelector(state=>state.productList)
+
+  const {CartItem}=ProductItem
+
+
+
+  const DecrementCounter=(quantity,ind)=>{
+    let qnt=quantity-1
+    if(qnt>0){
+    Dispatch(decrement({ind,qnt}))
+    }
+  }
+
+  const IncrementCounter=(quantity,ind)=>{
+    let qnt=quantity+1
+    Dispatch(increment({ind,qnt}))
+  }
+
+  const handleRemove=(product)=>{
+      Dispatch(remove(product))
+  }
+
+  const buyItem=()=>{
+    if(CartItem.length>=2){
+      alert("Your Order is Successfully Placed")
+    }
+    else{
+      alert("Please order atleast 2 items")
+    }
+  }
+
+  return (
+    <div className="cartContainer">
+      <h1 style={{textAlign:"center"}}>This is the Cart</h1>
+      {CartItem.map(item=>
+        <div className="cart-item">
+          <img style={{width:"80px", height:"80px",margin:"5px"}} src={item.img} alt={item.name}/>
+          
+            <h3>₹ {item.baseprice} / {item.priceType}</h3>
+
+            <div>
+            <button onClick={()=>DecrementCounter(item.quantity,item.id)}>-</button>
+            <b style={{margin:"20px"}}>Quantity:{item.quantity}</b>
+            <button onClick={()=>IncrementCounter(item.quantity,item.id)}>+</button>
+            </div>
+
+            <h3>Price:{item.price}</h3>
+
+            <button onClick={()=>handleRemove(item.id)}>Remove</button>
+            
+        </div>
+        )}
+
+      
+
+      <button className="btn" onClick={()=>buyItem()}>Buy Now</button>
+
+    </div>
+  )
+}
+
+export default Cart
